8652b6289fc4e97a65370d6e903df714dcbf67ed
Alexey Koshelev <koshelev2011@mail.ru>
2021-02-25 17:03:46 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadGroup mainGroup = new ThreadGroup("main group");
+        Thread Thread1 = new MyThread(mainGroup, "поток 1");
+        Thread Thread2 = new MyThread(mainGroup, "поток 2");
+        Thread Thread3 = new MyThread(mainGroup, "поток 3");
+        Thread Thread4 = new MyThread(mainGroup, "поток 4");
+        Thread1.start();
+        Thread2.start();
+        Thread3.start();
+        Thread4.start();
+
+
+        Thread.sleep(15000);
+        mainGroup.interrupt();
+
+
+    }
+}
